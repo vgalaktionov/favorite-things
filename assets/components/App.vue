f2e30264b9eebf2c51202d2335cd53a3f365ae66
Vadim <vadim.galaktionov@gmail.com>
2019-07-29 22:25:11 +0200

    add frontend skeleton

---
+++ assets/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">Hello {{ bundler }}</div>
+</template>
+
+<script>
+import Vue from "vue"
+
+export default {
+  data() {
+    return {
+      bundler: "Parcel"
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  color: green;
+}
+</style>
