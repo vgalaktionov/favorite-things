93411ead38c391b0e5f0d20bdec00678b9468bea
Vadim <vadim.galaktionov@gmail.com>
2019-07-29 21:59:23 +0200

    fix admin login

--- favorite_things/auth.py
+++ favorite_things/auth.py
@@ -10,17 +10,14 @@ class AdminBackend:
     def authenticate(self, request, username=None, password=None):
         """Hardcoded admin user."""
         if username == settings.ADMIN_USERNAME and password == os.environ.get('ADMIN_PASSWORD'):
-            return self.get_user()
+            user, _ = User.objects.get_or_create(
+                username=settings.ADMIN_USERNAME, defaults=dict(is_staff=True, is_superuser=True)
+            )
+            return user
         return None
 
-    def get_user(self, _id=None):
-        """Id is unused and just there to conform to the interface."""
-        user, _ = User.objects.get_or_create(
-            username=settings.ADMIN_USERNAME,
-            is_staff=True,
-            is_superuser=True
-        )
-        return user
+    def get_user(self, id):
+        return User.objects.get(id=id)
 
 
 class DummyBackend:
