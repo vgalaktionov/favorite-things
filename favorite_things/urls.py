9c09702060487cefba656ac3b03a2d2afa8b320e
Vadim <vadim.galaktionov@gmail.com>
2019-07-29 21:25:52 +0200

    wire up resources and auth

--- favorite_things/urls.py
+++ favorite_things/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import LogoutView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from things.resources import CategoryViewSet, ThingViewSet
+from .views import CustomLoginView, FrontendView
+
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet, 'categories')
+router.register('things', ThingViewSet, 'things')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('auth/login', CustomLoginView.as_view(), name='login'),
+    path('auth/logout', LogoutView.as_view(), name='logout'),
+    path('', FrontendView.as_view(), name='index'),
 ]
