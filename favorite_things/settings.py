66968e2ae560bfdba7e250da7dcbbb6258010c7d
Vadim <vadim.galaktionov@gmail.com>
2019-07-29 17:32:05 +0200

    add auditlog

--- favorite_things/settings.py
+++ favorite_things/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party
     'restframework',
+    'auditlog',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # audit
+    'auditlog.middleware.AuditlogMiddleware',
 ]
 
 ROOT_URLCONF = 'favorite_things.urls'
