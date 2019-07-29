d9087229a7a72e53335f0e7bf4a562238b07a5e1
Vadim <vadim.galaktionov@gmail.com>
2019-07-29 17:46:24 +0200

    initial model setup

--- things/models.py
+++ things/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.core.validators import MinLengthValidator, MinValueValidator
 
-# Create your models here.
+from auditlog.models import AuditlogHistoryField
+from auditlog.registry import auditlog
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Thing(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(null=True, blank=True, validators=[MinLengthValidator(10)])
+    ranking = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    metadata = JSONField(default=dict)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    audit_log = AuditlogHistoryField()
+
+    class Meta:
+        unique_together = ('category', 'ranking')
+
+
+auditlog.register(Thing, exclude_fields=['created_date', 'modified_date'])
