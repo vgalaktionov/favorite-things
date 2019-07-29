e5333bbbc675419586e7364fbf4f7fdb76fe753f
Vadim <vadim.galaktionov@gmail.com>
2019-07-29 17:52:02 +0200

    fix db settings

--- favorite_things/settings.py
+++ favorite_things/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'favorite_things.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-{
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'favorite-things'),
