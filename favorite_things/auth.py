1c499a6f695d00194981904e7762b35a26d71e1f
Vadim <vadim.galaktionov@gmail.com>
2019-08-04 18:17:03 +0200

    unused imports

--- favorite_things/auth.py
+++ favorite_things/auth.py
@@ -1,6 +1,3 @@
-import os
-
-from django.conf import settings
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -10,9 +7,7 @@ class DummyBackend:
     def authenticate(self, request, username=None, password=None):
         if username:
             user, _ = User.objects.get_or_create(
-                username=username,
-                is_staff=False,
-                is_superuser=False
+                username=username, is_staff=False, is_superuser=False
             )
             return user
         return None
