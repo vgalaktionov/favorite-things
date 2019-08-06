761d60711483a290f714f220ee97879c94e2cd08
Vadim <vadim.galaktionov@gmail.com>
2019-08-06 23:14:39 +0200

    implement re-sorting & fix jsonfield conflict

--- things/models.py
+++ things/models.py
@@ -1,5 +1,9 @@
+import json
+
 from django.contrib.auth import get_user_model
-from django.contrib.postgres.fields import JSONField
+
+# use the custom jsonfield to avoid conflicts with auditlog
+from jsonfield import JSONField
 from django.core.validators import MinLengthValidator, MinValueValidator
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
@@ -23,7 +27,7 @@ class Thing(models.Model):
         null=True, blank=True, validators=[MinLengthValidator(10)]
     )
     ranking = models.PositiveIntegerField(validators=[MinValueValidator(1)])
-    metadata = JSONField(default=dict, encoder=DjangoJSONEncoder)
+    metadata = JSONField(default=dict, encoder_class=DjangoJSONEncoder)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
@@ -40,13 +44,26 @@ class Thing(models.Model):
         return f"Thing '{self.title}' for user {self.user}"
 
     def save(self, *args, **kwargs):
-        """Add support for datetime in metadata."""
+        """
+        * Add support for datetime in metadata.
+        * Automatically re-rank within the same category.
+        """
+
         for key, value in self.metadata.items():
             if isinstance(value, str):
                 try:
                     self.metadata[key] = parse(value)
                 except ValueError:
                     continue
+
+        exists_with_ranking = Thing.objects.filter(
+            ranking=self.ranking, category=self.category
+        ).first()
+        if exists_with_ranking:
+            # recursively re-rank the other things
+            exists_with_ranking.ranking += 1
+            exists_with_ranking.save()
+
         return super().save(*args, **kwargs)
 
 
